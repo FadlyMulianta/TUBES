17eeb4bad8f33f5524a43763f0b9136fe3122d71
notimportantname <raget1812@gmail.com>
2025-04-21 02:17:30 +0700

    Artikel baru

---
+++ app/Filament/Admin/Resources/CategoryArticleResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\CategoryArticleResource\Pages;
+use App\Filament\Admin\Resources\CategoryArticleResource\RelationManagers;
+use App\Models\CategoryArticle;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoryArticleResource extends Resource
+{
+    protected static ?string $model = CategoryArticle::class;
+
+    protected static ?string $navigationGroup = 'Manajemen Artikel';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                ->required()
+                ->maxLength(225),
+
+                Forms\Components\FileUpload::make('icon')
+                ->required()
+                ->disk('public')
+                ->directory('asset/categoryartikel'),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\ImageColumn::make('icon')
+                ->circular()
+                ->height(50)
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategoryArticles::route('/'),
+            'create' => Pages\CreateCategoryArticle::route('/create'),
+            'edit' => Pages\EditCategoryArticle::route('/{record}/edit'),
+        ];
+    }
+}
